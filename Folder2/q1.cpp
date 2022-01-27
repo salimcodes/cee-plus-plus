df65c42345a147c1433540e7eab5273ced2e6939
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2022-01-27 12:33:00 +0100

    Add files via upload

---
+++ Folder2/q1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Write a structure tag and short main program that sets up a struct for a 
+Super Eagles player that contains his name, earned run average (ERA), 
+and team. In the program, declare one pitcher variable and pass its address to a function called Smokin’. 
+
+This function fills the pitcher variable with the following information:
+
+Name: Mikel Obi ERA: $ 10 million
+Team: Chelsea
+Once the data is in the structure, write the pitcher information to the screen.
+*/
+
+struct Pitcher
+{
+	char name[50];
+	char ERA[50];
+	char team[50];
+};
+
+void smokin(Pitcher*);
+int main()
+{
+	Pitcher variable3;
+	smokin(&variable3);
+
+	cout << "Name: " << variable3.name << " ERA: " << variable3.ERA << endl;
+	cout << "Team: " << variable3.team << endl;
+	system("Pause");
+}
+
+void smokin(Pitcher *variable1)
+{
+	Pitcher variable2;
+	strcpy(variable2.name, "Mikel Obi"); 
+	strcpy(variable2.ERA, "$ 10 million");
+	strcpy(variable2.team, "Chelsea");
+
+	*variable1 = variable2;
+}
+
