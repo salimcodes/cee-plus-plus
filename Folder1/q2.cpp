c23fcf35d3efbd2e240bf564b8054be7484b81e8
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2022-03-17 05:32:00 +0100

    Update q2.cpp

--- Folder1/q2.cpp
+++ Folder1/q2.cpp
@@ -5,9 +5,9 @@ using namespace std;
 Write a structure tag and short main program that sets up a struct for a baseball pitcher that
 contains his name, earned run average (ERA), 
 innings pitched and strikeout percentage. 
-In the program, declare one pitcher variable and pass its address to a function called Smokin’. 
+In the program, declare one pitcher variable and pass its address to a function called Smokinâ€™. 
 This function fills the pitcher variable with the following information:
-Name: Smokin’ Joe Green ERA: 1.2
+Name: Smokinâ€™ Joe Green ERA: 1.2
 Innings pitched: 55 Strikeout percentage: 0.25
 Once the data is in the structure, write the pitcher information to the screen.
 */
@@ -41,3 +41,7 @@ void smokin(Pitcher *variable1)
 
 	*variable1 = variable2;
 }
+
+
+//Twitter @SalimOpines: https://twitter.com/SalimOpines
+//Github: https://www.github.com/salimcodes
